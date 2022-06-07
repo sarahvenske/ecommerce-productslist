fbe66e8e36635c8f31cba5fd19a93bc80775123a
Sarah Venske Costa <sarahvenske@gmail.com>
2022-06-06 22:40:25 -0300

    continuação - refatoração

--- src/js/dataProdutos.js
+++ src/js/dataProdutos.js
@@ -1,37 +1,81 @@
 const produtos = [
     {
+        id: 1,
         nome: 'Banana',
         preco: 2,
         secao: 'Hortifruti',
         categoria: 'fruta',
-        img: "./src/img/banana2.jpeg"
+        img: "./src/img/banana2.jpeg",
+        promocao: true,
+        precoPromocao: 1,
+        componentes: [
+            ' Potássio',
+            ' Vitamina B6',
+            ' Vitamina C',
+            ' Folatos'
+        ]         
     },
     {
+        id: 2,
         nome: 'Morango',
         preco: 2,
         secao: 'Hortifruti',
         categoria: 'fruta',
-        img: './src/img/morango2.jpeg'
+        img: './src/img/morango2.jpeg',
+        promocao: false,
+        precoPromocao: 0,
+        componentes: [
+            ' Fibras',
+            ' Vitamina C',
+            ' Cálcio',
+            ' Ferro'
+        ] 
     },
     {
+        id: 3,
         nome: 'Maçã',
         preco: 2,
         secao: 'Hortifruti',
         categoria: 'fruta',
-        img: './src/img/maca2.jpeg'
+        img: './src/img/maca2.jpeg',
+        promocao: true,
+        precoPromocao: 1.50,
+        componentes: [
+            ' Potássio',
+            ' Vitamina A',
+            ' Vitamina E',
+            ' Vitamina C'
+        ] 
     },
     {
+        id: 4,
         nome: 'Pão',
         preco: 4,
         secao: 'Panificadora',
         categoria: 'Pães',
-        img: './src/img/pao2.jpeg'
+        img: './src/img/pao2.jpeg',
+        promocao: true,
+        precoPromocao: 2.50,
+        componentes: [
+            ' Cálcio',
+            ' Sódio',
+            ' Fibra Alimentar',
+            ' Proteínas'
+        ]
     },
     {
+        id: 5,
         nome: 'Leite',
         preco: 5,
         secao: 'Laticínio',
         categoria: 'Leite',
-        img: './src/img/leite2.jpeg'
+        img: './src/img/leite2.jpeg',
+        promocao: false,
+        precoPromocao: 1.00,
+        componentes: [
+            ' Carboidratos',
+            ' Proteínas',
+            ' Gorduras totais'
+        ] 
     }
 ]
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 const sectionCards = document.querySelector('.section_cards')
 
+let arrayProdutosSelecionados = []
+
 
 //Criação dinâmica dos cards de produtos:
 
@@ -27,41 +29,36 @@ function renderCard(listaProdutos){
         const tag = document.createElement('div')
         tag.className = "tag"
         tag.innerText = listaProdutos[i].secao
+
+        const nutrientes = document.createElement('div')
+        nutrientes.className = "nutrientes"
+        nutrientes.innerText = listaProdutos[i].componentes
         
+        const span = document.createElement('span')
+        span.className = "span_card"
+
         const preco = document.createElement('div')
         preco.className = "preco"
         preco.innerText = listaProdutos[i].preco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
-        
-        divImg.append(img)
-        card.append(divImg, nome, tag, preco)
-        ul.append(card)
-        
-        //Função soma - total de valores produtos selecionados
-        
-        const total = document.querySelector('.box_total_value')
-        total.innerText = "R$0,00"
-        
-        function valorTotal(listaProdutos){
-            
-            let soma = 0
-            total.innerHTML = " "
-            
-            for(let i=0; i< listaProdutos.length; i++){
-                soma += listaProdutos[i].preco
-            }
-        
-            const valorSoma = document.createElement('div')
-            valorSoma.className = "valorSoma"
-            valorSoma.innerText = soma.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
-    
-            total.append(valorSoma)
-        
-        }
-    
-        valorTotal(listaProdutos)
 
-    }
-    
+        const botaoComprar = document.createElement('button')
+        botaoComprar.className = "botao_comprar"
+        botaoComprar.innerText = "Comprar"
+
+        divImg.append(img)
+        span.append(preco, botaoComprar)
+        card.append(divImg, nome, tag, nutrientes, span)
+        ul.append(card)
+
+          //Evento adicionado ao card e modelagem dos dados no carrinho:
+        
+        function addProduto (){
+            arrayProdutosSelecionados.push(listaProdutos[i])
+            valorTotal()
+            renderizarCart()
+        }
+        card.addEventListener("click", addProduto)
+    } 
     sectionCards.append(ul)
 }
 
@@ -71,33 +68,36 @@ renderCard(produtos)
 //Filtro tag Todos os Produtos:
 
 function retornarTodos(){
-
+    
     sectionCards.innerHTML = ""
-
+    
     const produtosFiltrados = produtos.filter((produto) => {
         return produtos
     })
     renderCard(produtosFiltrados) 
 }
 
-const tagTodosProdutos = document.querySelector("#btn_todos_produtos")
+const tagTodosProdutos = document.querySelector('#btn_todos_produtos')
 tagTodosProdutos.addEventListener("click", retornarTodos)
+
+
 
 //Filtro produtos tag Hortifrutti:
 
 function filtrarHortifruti(){
-   
+    
     sectionCards.innerHTML = ""
-
+    
     const produtosFiltrados = produtos.filter((produto) => {
         return produto.secao === "Hortifruti"
     })
     renderCard(produtosFiltrados) 
-
+    
 }
 
-const tagHortifruti = document.querySelector("#btn_hortifruti")
+const tagHortifruti = document.querySelector('#btn_hortifruti')
 tagHortifruti.addEventListener("click", filtrarHortifruti)
+
 
 
 //Filtro produtos tag Panificadora:
@@ -105,15 +105,16 @@ tagHortifruti.addEventListener("click", filtrarHortifruti)
 function filtrarPanificadora(){
     
     sectionCards.innerHTML = ""
-
+    
     const produtosFiltrados = produtos.filter((produto) => {
         return produto.secao === "Panificadora"
     })
     renderCard(produtosFiltrados) 
 }
 
-const tagPanificadora = document.querySelector("#btn_panificadora")
+const tagPanificadora = document.querySelector('#btn_panificadora')
 tagPanificadora.addEventListener("click", filtrarPanificadora)
+
 
 
 //Filtro produtos tag Laticínios:
@@ -121,33 +122,157 @@ tagPanificadora.addEventListener("click", filtrarPanificadora)
 function filtrarLaticinios(){
     
     sectionCards.innerHTML = ""
-
+    
     const produtosFiltrados = produtos.filter((produto) => {
         return produto.secao === "Laticínio"
     })
     renderCard(produtosFiltrados) 
 }
 
-const tagLaticinios = document.querySelector("#btn_laticinios")
+const tagLaticinios = document.querySelector('#btn_laticinios')
 tagLaticinios.addEventListener("click", filtrarLaticinios)
 
-//Busca campo pesquisa:
+
+
+//Busca campo pesquisa (nome, categoria, seção):
+
+const searchButton = document.querySelector(".search_button")
 
 function search(){
     
     sectionCards.innerHTML = ""
-
-    let input = document.querySelector(".search_field")
+    
+    let input = document.querySelector('.search_field')
     let value = input.value;
     
-    let filtro = produtos.filter((texto) => {
+    let filtroName = produtos.filter((texto) => {
         return value.toLowerCase() === texto.nome.toLowerCase()
     })
     
-    renderCard(filtro)
+    let filtroCategory = produtos.filter((texto) => {
+        return value.toLowerCase() === texto.categoria.toLowerCase()
+    })
+    
+    let filtroSection = produtos.filter((texto) => {
+        return value.toLowerCase() === texto.secao.toLowerCase()
+    })
+    
+    renderCard(filtroName)
+    renderCard(filtroCategory)
+    renderCard(filtroSection)
 }
 
-const searchButton = document.querySelector(".search_button")
 searchButton.addEventListener("click", search)
 
 
+
+//Listar itens no Carrinho:
+
+const ulCart = document.querySelector('.cart_render_list')
+
+function renderizarCart(){
+    
+    ulCart.innerHTML = ""
+    
+    if(arrayProdutosSelecionados.length == 0){
+        ulCart.innerText = "Por enquanto não há produtos no carrinho."
+    }else{
+        
+        for(let i = 0; i < arrayProdutosSelecionados.length; i++){
+            
+            const cardCarrinho = document.createElement('li')
+            cardCarrinho.className = "card_carrinho"
+            
+            const divEsquerda = document.createElement('div')
+            divEsquerda.className = "div_esquerda"
+            
+            const imgCarrinho = document.createElement('img')
+            imgCarrinho.className = "img_carrinho"
+            imgCarrinho.src = arrayProdutosSelecionados[i].img
+            
+            const divDireita = document.createElement('div')
+            divDireita.className = "div_direita"
+            
+            const div1 = document.createElement('div')
+            div1.className = "div1"
+            
+            const nomeCarrinho = document.createElement('div')
+            nomeCarrinho.className = "nome_carrinho"
+            nomeCarrinho.innerText = arrayProdutosSelecionados[i].nome
+            
+            const botaoExcluir = document.createElement('button')
+            botaoExcluir.className = "btn_excluir"
+            botaoExcluir.innerText = "X"
+            
+            const secaoCarrinho = document.createElement('div')
+            secaoCarrinho.className = "secao_carrinho"
+            secaoCarrinho.innerText = arrayProdutosSelecionados[i].secao
+            
+            const precoCarrinho = document.createElement('div')
+            precoCarrinho.className = "preco_carrinho"
+            precoCarrinho.innerText = arrayProdutosSelecionados[i].preco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+            
+            div1.append(nomeCarrinho, botaoExcluir)
+            divEsquerda.append(imgCarrinho)
+            divDireita.append(div1, secaoCarrinho, precoCarrinho)
+            cardCarrinho.append(divEsquerda, divDireita)
+            ulCart.append(cardCarrinho)
+            
+
+            //Remover Produto Carrinho:
+            
+            function removerProduto(event){
+                const button = event.target    
+                arrayProdutosSelecionados.splice(button.id,1)
+                
+                renderizarCart()
+                valorTotal()  
+            }
+            botaoExcluir.addEventListener("click", removerProduto)
+        }
+    }
+}
+renderizarCart()
+    
+//Função Totais (quantidade e valor dos produtos selecionados)
+
+const boxQuantidade = document.querySelector('.cart_box_quantity')
+const boxSoma = document.querySelector('.cart_box_value')
+
+
+function valorTotal(){
+    
+    let soma = 0
+  
+    boxQuantidade.innerHTML = ""
+    boxSoma.innerHTML = ""
+    
+    if(arrayProdutosSelecionados.length > 0){
+        
+        for(let i = 0; i < arrayProdutosSelecionados.length; i++){
+        soma += arrayProdutosSelecionados[i].preco
+        }
+    
+    const textoQde = document.createElement('div')
+    textoQde.innerText = "Quantidade:"
+    
+    const qdeTotal = document.createElement('div')
+    qdeTotal.className = "qde_total"
+    qdeTotal.innerText = arrayProdutosSelecionados.length
+
+    const textoTotal = document.createElement('div')
+    textoTotal.innerText = "Total:"
+
+    const vlrTotal = document.createElement('div')
+    vlrTotal.className = "vlr_total"
+    vlrTotal.innerText = soma.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+    
+    boxQuantidade.append(textoQde, qdeTotal)
+    boxSoma.append(textoTotal, vlrTotal)
+
+    }
+}
+
+valorTotal()
+    
+    
